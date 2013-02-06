05c86ac95aace088051454aa91657b6a944ad511
Laser Nite <lasernite@gmail.com>
2013-02-06 15:32:06 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YogaPond::Application.config.secret_token = '032d1810837ca87a471180465a82d9f9dc48d8ae07f1b7454e80025a643c8c3a3b508f25f409d835cba9b2848842c223bb9aceac37ac8c66faff7b687882565d'
